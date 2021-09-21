327f5b958b015b9ab5e5afded8e29148fd9ba953
Matheus M. Reis <mmr94@live.com>
2021-09-21 11:06:17 -0300

    Código com o Padrão Adapter aplicado

---
+++ DesignPatternsII/src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,29 @@
+package br.com.alura.loja.orcamento;
+
+import java.util.Map;
+
+import br.com.alura.loja.DomainException;
+import br.com.alura.loja.http.HttpAdapter;
+
+public class RegistroDeOrcamento {
+
+	private HttpAdapter http;
+
+	public RegistroDeOrcamento(HttpAdapter http) {
+		super();
+		this.http = http;
+	}
+
+	public void registrar(Orcamento orcamento) {
+		if (!orcamento.isFinalizado()) {
+			throw new DomainException("Orçamento não finalizado");
+		}
+
+		String url = "http://api.externa/orcamento";
+		Map<String, Object> dados = Map.of("valor", orcamento.getValor(),
+				"orcamento", orcamento.getQuantidadeItens());
+
+		http.post(url, dados);
+	}
+
+}
